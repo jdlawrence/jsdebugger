d9471bb110bdd803eaa5329953dcc6f5604de48f
jdlawrence <jamil.lawrence@gmail.com>
2016-06-21 14:15:31 -0700

    Updating JS file

--- writeJSHere.js
+++ writeJSHere.js
@@ -1,15 +1,3 @@
-function geoMean(arr) {
-  var prod = product(arr);
-  var result = Math.pow(prod, 1 / arr.length);
-  return result; 
-}
+// Write your amazing JS here
+// Test it out by opening "openInBrowser.html" in Chrome
 
-function product(arr) {
-  return arr.reduce(function(a, b){
-    return a * b;
-  });
-}
-var prodResult = product([1,2,3,4]);
-
-console.log(prodResult);
-console.log(geoMean([2, 8, 512, 7, 37 , 7]));
